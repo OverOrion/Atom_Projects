a8601d610cba508fdc73bd0db3b9606870ced766
OverOrion <szilard.parrag@gmail.com>
2017-08-22 19:51:19 +0200

    Pythagoras, Pythagorean triangle, nested if

---
+++ Exercises/Beginner_Projects/Pythagorean_Triples_checker_including_triangle_checking.py
@@ -0,0 +1,15 @@
+print ("Please input side a, b, and c, where c is the longest.")
+
+print("Please input side a.")
+a=float(input())
+print("Please input side b.")
+b=float(input())
+print("Please input side c (aka longest side).")
+c=float(input())
+print ("Please input side a, b, and c, where c is the longest.")
+if (c**2) == (a**2 + b**2):
+    print ("This is a Pythagorean triangle.")
+elif a<b+c and b<a+c and c<a+c:
+    print ("This is a triangle.")
+else:
+    print ("This is NOT even a triangle.")
