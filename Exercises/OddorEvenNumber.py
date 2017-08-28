d50a6b641791570e988570e960ef51d22e9dc361
OverOrion <szilard.parrag@gmail.com>
2017-08-28 11:24:36 +0200

    program to determine whetever it is an odd or even number (just4fun) :]

---
+++ Exercises/OddorEvenNumber.py
@@ -0,0 +1,6 @@
+number = int(input("Please enter any number."))
+
+if number % 2 == 0:
+    print(str(number) + " is an even number.")
+elif number % 2 == 1:
+    print(str(number) + " is an odd number.")
