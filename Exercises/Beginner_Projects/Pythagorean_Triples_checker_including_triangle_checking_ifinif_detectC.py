cf127e6d77dc5e821b00f605c875085e06017b2e
OverOrion <szilard.parrag@gmail.com>
2017-08-29 13:45:41 +0200

    using functions to be future proof

--- Exercises/Beginner_Projects/Pythagorean_Triples_checker_including_triangle_checking_ifinif_detectC.py
+++ Exercises/Beginner_Projects/Pythagorean_Triples_checker_including_triangle_checking_ifinif_detectC.py
@@ -1,15 +1,27 @@
 print("Please input side a, b, and c.")
 
-def triangle_sides_shorter():
+def side_sorter():
+    global a
+    global b
+    global c
     sides = []
-    while len(sides) != 3:
-        sides.append = int(input())
+    for i in range(3):
+        sides.append(int(input()))
+    hypotenuse = max(sides)
+    c = hypotenuse
+    sides.remove(c)
+    a = sides[0]
+    sides.remove(a)
+    b = sides[0]
 
 def Pythagorean_triangle_checker():
     if a<b+c and b<a+c and c<a+c:
         if (c**2) == (a**2 + b**2):
-        print("This is a Pythagorean triangle.")
-        exit()
-    print("This is a triangle.")
-    else:
-    print("This is NOT even a triangle.")
+            print("This is a Pythagorean triangle.")
+            exit()
+            print("This is a triangle.")
+        else:
+            print("This is NOT even a triangle.")
+
+side_sorter()
+Pythagorean_triangle_checker()
